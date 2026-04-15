35e082bebff3b3ab34d03db6752ccf9d9d9297b7
Ivan Cvetanovic <icvetanovic99@gmail.com>
2026-04-15 12:57:05 +0200

    New readme

--- app_modules/pipeline.py
+++ app_modules/pipeline.py
@@ -1,12 +1,12 @@
-import os
 import classla
+import torch
 
 _nlp = None
 
 def get_nlp():
     global _nlp
     if _nlp is None:
-        use_gpu = os.getenv("CLASSLA_GPU", "0").strip() == "1"
+        use_gpu = torch.cuda.is_available()
         _nlp = classla.Pipeline(
             lang="sr",
             processors="tokenize,pos,lemma,ner,depparse",
